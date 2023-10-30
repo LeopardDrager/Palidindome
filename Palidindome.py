00c16d0ada68b6faa300f18276e8699070f742cc
Maguire Mosby <136644201+LeopardDrager@users.noreply.github.com>
2023-10-29 19:21:15 -0500

    Add files via upload

---
+++ Palidindome.py
@@ -0,0 +1,11 @@
+a = (input("Enter a value! ")) #Just asking for user input
+
+reverse = a[::-1]# I learn't this tool before and thought of it as I was messing with a different python expieriment in my free time and I learnt that this will litteraly just put something in reverse order
+
+if( a == reverse ): 
+    print("Yes it is palindrome! :)")
+else:
+    print("No it is not a palindrome! :(")
+    
+
+    
